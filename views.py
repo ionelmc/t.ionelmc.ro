e7803b9a87545a1d443bea8d8b2cba206e4c8d81
Ionel Cristian Maries <contact@ionelmc.ro>
2013-10-31 19:25:25 +0200

    Use GA Measurement Protocol instead.

--- views.py
+++ views.py
@@ -1,31 +1,53 @@
-from pyga.requests import Tracker, Page, Session, Visitor, PageViewRequest
+from time import time
+from uuid import uuid4
+from urllib import urlencode
+from Cookie import SimpleCookie
+from werkzeug.urls import url_decode
+#from werkzeug.debug import DebuggedApplication
 
 def track(env, start_response):
     try:
-        gaid, domain, page = env['PATH_INFO'].lstrip('/').split('/', 2)
+        gaid, domain = env['PATH_INFO'].lstrip('/').split('/', 1)
+        if '/' in domain:
+            raise ValueError
     except ValueError:
         return """
             <html><body><h1>Usage:</h1>
-            <code>%(host)s/UA-123456/domain.com/path/to/page?http://domain/referral/path</code>
-            <blockquote>or</blockquote>
-            <code>%(host)s/UA-123456/domain.com/path/to/page?http://domain/referral/path<code>
+            <code>%(host)s/UA-123456/domain.com?dp=<b>/path/to/page</b>&t=pageview&</code>
+
             </body></html>
         """ % {'host': env.get('HTTP_HOST', '')}
 
-    tracker = Tracker(gaid, domain)
-    tracker.config.queue_requests = True
-    visitor = Visitor()
-    visitor.extract_from_server_meta(env)
-    page = Page('/' + page)
-    page.referrer = env.get("QUERY_STRING") or None
-    request = PageViewRequest(
-        config=tracker.config,
-        tracker=tracker,
-        visitor=visitor,
-        session=Session(),
-        page=page,
+    cookies = SimpleCookie()
+    if 'HTTP_COOKIE' in env:
+        cookies.load(env['HTTP_COOKIE'])
+
+    cookies['cid'] = cid = 'cid' in cookies and cookies['cid'].value or str(uuid4())
+    cookies['cid']['path'] = '/%s/%s' % (gaid, domain)
+    cookies['cid']['max-age'] = 31536000
+
+    data = dict(
+        v=1,
+        tid=gaid,
+        cid=cid,
+        dh=domain,
+        dr=env.get("HTTP_REFERER", ''),
+        z=str(time()),
     )
+    data.update(url_decode(env["QUERY_STRING"]).items())
+
+
+    location = "//www.google-analytics.com/collect?" + urlencode(data)
+    #start_response('200', [
+    #    ('Set-Cookie', cookies['cid'].OutputString()),
+    #])
+    #return '<a href="%s?%s">x</a><code>%r</code><br><iframe src="%s"/>' % (
+    #    env['PATH_INFO'], env["QUERY_STRING"],
+    #    location, location
+    #)
+
     start_response('307 Temporary Redirect', [
-        ('Location', request.build_http_request().get_full_url())
+        ('Set-Cookie', cookies['cid'].OutputString()),
+        ('Location', location)
     ])
     return ""
